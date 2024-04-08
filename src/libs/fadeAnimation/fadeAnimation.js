5ce1a4d57adf23ec5188292a2e61cfe98ec17693
Aleksey Kobychev <alekskob33@yandex.ru>
2024-04-09 00:20:23 +0300

    fix: init opacity=0 for auto-fade animations

--- src/libs/fadeAnimation/fadeAnimation.js
+++ src/libs/fadeAnimation/fadeAnimation.js
@@ -1,7 +1,11 @@
 export default class FadeAnimation {
   fadeIn(elem) {
-    elem.style.transition = 'opacity 300ms 50ms ease-in';
-    elem.style.opacity = 1.0;
+    elem.style.opacity = 0; // init
+
+    setTimeout(() => {
+      elem.style.transition = 'opacity 300ms 0ms ease-in';
+      elem.style.opacity = 1.0;
+    }, 0);
   }
   fadeOut(elem) {
     elem.style.opacity = 0.0;
