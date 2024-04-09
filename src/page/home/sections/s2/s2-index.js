98957505fa6b6a36ca2e06f02d6ceb4366a3aaa3
Aleksey Kobychev <alekskob33@yandex.ru>
2024-04-10 00:12:44 +0300

    feat: add smooth effect when running-tape appears

--- src/page/home/sections/s2/s2-index.js
+++ src/page/home/sections/s2/s2-index.js
@@ -1,11 +1,25 @@
 import './s2.style.sass';
 import Tape from '../../../../libs/running-tape/js/tape.js';
 import '../../../../libs/running-tape/css/style.css';
+const tapeContainer = document.querySelector('.header-tape');
 
-const tape = new Tape({
-  rootElem: document.querySelector('.header-tape'),
-  cssGap: '0',
-  speed: 1.3,
-});
+if (tapeContainer) {
+  showSmoothly();
 
-// tape.start();
+  const tape = new Tape({
+    rootElem: tapeContainer,
+    cssGap: '0',
+    speed: 1.3,
+  });
+
+  tape.start();
+}
+
+function showSmoothly() {
+  tapeContainer.style.opacity = 0;
+
+  setTimeout(() => {
+    tapeContainer.style.transition = 'opacity 500ms';
+    tapeContainer.style.opacity = 1;
+  }, 500);
+}
