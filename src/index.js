25aa690f90d611efc5013b7227c671ca2ba048db
Aleksey Kobychev <alekskob33@yandex.ru>
2024-04-11 00:16:36 +0300

    feat: add large-title

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import './style.sass';
 import './page/home/sections/s1/module.js';
 import './page/home/sections/s2/s2-index.js';
 import './page/home/sections/s3/s3-index.js';
+import './page/home/sections/s4/s4-index.js';
