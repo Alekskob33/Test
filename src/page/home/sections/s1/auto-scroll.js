e8a1e19771ae9ff9502a9e19e8686c98c1f14f1a
Aleksey Kobychev <alekskob33@yandex.ru>
2024-04-11 22:07:40 +0300

    feat: add auto-scroll on video-elem click

--- src/page/home/sections/s1/auto-scroll.js
+++ src/page/home/sections/s1/auto-scroll.js
@@ -1,9 +1,15 @@
 const playBtn = document.querySelector('.play-button');
+const smallVideoContainer = document.querySelector('#small-video');
 
 (() => {
-  if (!playBtn) return;
+  if (!playBtn || !smallVideoContainer) return;
 
-  playBtn.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  // Auto-scroll on click play
+  [playBtn, smallVideoContainer].forEach((elem) => {
+    elem.addEventListener('click', autoScrollUp);
   });
 })();
+
+function autoScrollUp() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
