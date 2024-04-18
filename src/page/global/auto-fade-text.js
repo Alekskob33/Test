943c329add7e02f85b2f70942bad785bdb396da8
Aleksey Kobychev <alekskob33@yandex.ru>
2024-04-18 19:18:28 +0300

    feat: add auto-fade to other texts

--- src/page/global/auto-fade-text.js
+++ src/page/global/auto-fade-text.js
@@ -3,12 +3,12 @@ import { observeElements } from '../../libs/observe-elements.js';
 
 const autoFadeElements = document.querySelectorAll('.js-auto-fade');
 const options = {
-  threshold: 0.5,
+  threshold: 0.3,
 };
 
 // Auto-fade elements within viewport
 const observer = observeElements(autoFadeElements, {
-  onAppear: (el) => fadeIn(el, '300ms 0ms ease-in'),
+  onAppear: (el) => fadeIn(el, '900ms 0ms ease-in'),
   onDisappear: (el) => fadeOut(el, '300ms 0ms ease-out'),
   options,
 });
