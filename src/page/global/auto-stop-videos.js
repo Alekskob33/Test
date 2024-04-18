15fe9fcb0cc9ba668b2b106f6974827e85c7b359
Aleksey Kobychev <alekskob33@yandex.ru>
2024-04-18 18:06:26 +0300

    fix: add delay before start observing videos

--- src/page/global/auto-stop-videos.js
+++ src/page/global/auto-stop-videos.js
@@ -1,21 +1,27 @@
+import { afterDelay } from '../../libs/delay.js';
 import { observeElements } from '../../libs/observe-elements.js';
 import { showcaseManager } from '../home/sections/s1/module.js';
 
 const videoCollection = document.querySelectorAll('video');
 
-observeElements(videoCollection, {
-  onAppear: (elem) => {
-    if (elem.matches('#main-video')) return;
-    elem.play();
-  },
-  onDisappear: (elem) => {
-    if (elem.matches('#main-video')) {
-      showcaseManager.activateDefaultState();
-      // showcaseManager.mainPlayer.currentTime = 0;
-    }
-    elem.pause();
-  },
-  options: {
-    threshold: 0.5,
-  },
+// * (a)
+afterDelay(200, () => {
+  observeElements(videoCollection, {
+    onAppear: (elem) => {
+      if (elem.matches('#main-video')) return;
+      elem.play();
+    },
+    onDisappear: (elem) => {
+      if (elem.matches('#main-video')) {
+        showcaseManager.activateDefaultState();
+        // showcaseManager.mainPlayer.currentTime = 0;
+      }
+      elem.pause();
+    },
+    options: {
+      threshold: 0.5,
+    },
+  });
 });
+
+// * (a) avoid immediate play/stop when page has scrolled position on start
