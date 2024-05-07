e35c611899a7198c9786d9bf7b965b5d939ca0f2
Aleksey Kobychev <alekskob33@yandex.ru>
2024-05-07 18:46:58 +0300

    feat: comment all 'sections' js-code

--- src/index.js
+++ src/index.js
@@ -15,14 +15,14 @@ import './page/global/setVh.js';
 import './page/global/preloader/page-preloader.js';
 
 /* Home */
-import './page/home/sections/s1/module.js';
-import './page/home/sections/s2/s2-index.js';
-import './page/home/sections/s3/s3-index.js';
-import './page/home/sections/s4/s4-index.js';
-import './page/home/sections/s5/s5-index.js';
-import './page/home/sections/s6/s6-index.js';
-import './page/home/sections/s7/s7-index.js';
-import './page/home/sections/s8/s8-index.js';
+// import './page/home/sections/s1/module.js';
+// import './page/home/sections/s2/s2-index.js';
+// import './page/home/sections/s3/s3-index.js';
+// import './page/home/sections/s4/s4-index.js';
+// import './page/home/sections/s5/s5-index.js';
+// import './page/home/sections/s6/s6-index.js';
+// import './page/home/sections/s7/s7-index.js';
+// import './page/home/sections/s8/s8-index.js';
 
 /* Case Study */
 import './page/second-page/sections/header/ps1-index.js';
