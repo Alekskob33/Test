59ce862efb628f9a2e1f60c2b663ec14a5168975
Aleksey Kobychev <alekskob33@yandex.ru>
2024-05-10 19:53:12 +0300

    fix: exclude css duplicates

--- src/index.js
+++ src/index.js
@@ -23,5 +23,3 @@ import './page/second-page/sections/header/ps1-index.js';
 import './page/second-page/sections/brief/ps2-index.js';
 import './page/second-page/sections/article/ps3-index.js';
 import './page/second-page/sections/footer/footer.js';
-
-// import './CustomStylesFromMaks/main.css';
