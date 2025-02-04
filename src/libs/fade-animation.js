78372adc341111a1b56a159854d4662232315c44
Aleksey Kobychev <alekskob33@yandex.ru>
2025-02-04 20:18:14 +0300

    fix: assignation '0 !important' does not work in js

--- src/libs/fade-animation.js
+++ src/libs/fade-animation.js
@@ -1,12 +1,15 @@
 export function fadeIn(elem, optionsString = '300ms ease-in 0ms') {
-  elem.style.opacity = 0; // init
+  elem.classList.add('fadeOut');
+  elem.classList.remove('fadeIn');
   elem.offsetHeight; // restart animation
 
   elem.style.transition = `opacity ${optionsString}`;
-  elem.style.opacity = 1.0;
+  elem.classList.remove('fadeOut');
+  elem.classList.add('fadeIn');
 }
 
 export function fadeOut(elem, optionsString) {
   elem.style.transition = `opacity ${optionsString}`;
-  elem.style.opacity = 0;
+  elem.classList.remove('fadeIn');
+  elem.classList.add('fadeOut');
 }
