4e203947d52b67bf2943bbb3878ecd26d5a00e62
Aleksey Kobychev <alekskob33@yandex.ru>
2025-02-05 21:37:56 +0300

    feat: change video initialization way

--- src/page/home/sections/s3/video/initDefaultVideo.js
+++ src/page/home/sections/s3/video/initDefaultVideo.js
@@ -1,8 +1,10 @@
 export function initDefaultVideo(video) {
   video.muted = true;
   video.setAttribute('muted', true);
+
   video.setAttribute('playsinline', true);
-  video.setAttribute('autoplay', true);
-  video.setAttribute('loop', true);
-  video.play();
+  video.autoplay = true;
+  video.loop = true;
+
+  // video.play();
 }
